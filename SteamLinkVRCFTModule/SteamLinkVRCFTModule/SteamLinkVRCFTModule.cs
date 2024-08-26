a8668b4ab007883365110fc879a08620d5da848e
Hash <stephane.jourdan138@gmail.com>
2024-08-27 01:00:26 +0200

    Updated Steam logo & changed moduleinfo name
    Changed SteamLink logo to comply with Valve's guidelines alongside with a change to the module's displayed name

--- SteamLinkVRCFTModule/SteamLinkVRCFTModule/SteamLinkVRCFTModule.cs
+++ SteamLinkVRCFTModule/SteamLinkVRCFTModule/SteamLinkVRCFTModule.cs
@@ -16,7 +16,7 @@ namespace SteamLinkVRCFTModule
 
         public override (bool eyeSuccess, bool expressionSuccess) Initialize(bool eyeAvailable, bool expressionAvailable)
         {
-            ModuleInformation.Name = "SteamLink VRCFT Module";
+            ModuleInformation.Name = "SteamLink Module";
 
             var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("SteamLinkVRCFTModule.Assets.steamlink.png");
             ModuleInformation.StaticImages = stream != null ? new List<Stream> { stream } : ModuleInformation.StaticImages;
